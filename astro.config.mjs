ff85fb29e8867974fadaf5a33b5217f0468f7f13
Nicolas Rollier <nrollr@users.noreply.github.com>
2024-03-16 17:38:31 +0100

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://instancetyp.es/',
   integrations: [tailwind(), sitemap()]
 });
