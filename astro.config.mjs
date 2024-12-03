52771aed295e99712f4e0fd7132461548e15466a
Nicolas Rollier <nrollr@users.noreply.github.com>
2024-12-03 20:27:42 +0100

    Added P5en and Trn2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import db from "@astrojs/db";
+// import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://instancetyp.es/',
-  integrations: [tailwind(), sitemap(), db()]
+  integrations: [tailwind(), sitemap()]
 });
