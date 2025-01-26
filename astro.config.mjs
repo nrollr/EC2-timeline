e3af738044732906630b9f42441165471cd86754
Nicolas Rollier <developer@atelier19.be>
2025-01-26 14:40:01 +0100

    Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 // import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: 'https://instancetyp.es/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [sitemap()]
 });
